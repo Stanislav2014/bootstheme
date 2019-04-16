0896be7f14da62ee06623bf31ea2182780df8d87
Stanislav <s_admin@mail.ru>
2019-04-16 22:37:20 +0300

    efit

--- index.php
+++ index.php
@@ -18,18 +18,18 @@ get_header();
         <form method="post">
             <div class="form-row">
                 <div class="col-5 col-xl-5">
-                    <div class="form-group">
-                        <label for="exampleInputName">Имя</label>
+                    <div class="form-group forminput">
+                        <label for="exampleInputName" class="formlabel">Имя</label>
                         <input type="text" class="form-control" id="exampleInputName" placeholder="Вася">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group forminput">
                         <label for="exampleInputEmail">E-Mail</label>
                         <input type="email" class="form-control" id="exampleInputEmail">
                     </div>
                 </div>
 
                 <div class="col-6 col-xl-6 ml-auto">
-                    <div class="form-group">
+                    <div class="form-group forminput">
                         <label for="exampleInputComment">Комментарии</label>
                         <textarea class="form-control" rows=4 id="exampleInputComment"></textarea>
 
@@ -40,7 +40,7 @@ get_header();
 
             <div class="row">
                 <div class="col-3 col-xl-3 offset-9 offset-xl-9">
-                    <button type="submit" class="btn btn-primary btn-block">Записать</button>
+                    <button type="submit" class="btn btn-block submit">Записать</button>
                 </div>
 
             </div>
@@ -50,7 +50,7 @@ get_header();
 
 </header>
 
-<section id="comment" class="comment">
+<section id="posts" class="posts">
     <div class="container">
         <div class="col-12 col-lg-12">
             <h1 align="center">
